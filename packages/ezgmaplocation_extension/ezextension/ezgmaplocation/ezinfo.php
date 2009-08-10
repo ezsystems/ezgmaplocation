78298c0da48206dcd8bdb9388389f152caa5f18e
André Rømcke <ar@ez.no>
2009-08-10 16:23:10 +0000

    - Added ezinfo and License file
    git-svn-id: http://svn.projects.ez.no/ezgmaplocation/trunk@20 a5a2e000-4f5a-4d95-8ad8-c7c64b196f82

---
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/ezinfo.php
@@ -0,0 +1,38 @@
+<?php
+//
+// Definition of eZStarRatingInfo class
+//
+// SOFTWARE NAME: eZ Google Maps Location
+// SOFTWARE RELEASE: 1.0
+// COPYRIGHT NOTICE: Copyright (C) 2009 eZ Systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+
+class eZGmapLocationInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "eZ Google Maps Location",
+            'Version' => "1.0",
+            'Copyright' => "Copyright (C) 2008 Joe Kepley, 2009 eZ Systems AS",
+            'License' => "GNU General Public License v2.0",
+        );
+    }
+}
+?>
