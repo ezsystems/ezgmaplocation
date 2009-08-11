37133abd198a247b7181a8256818277338b08e1e
André Rømcke <ar@ez.no>
2009-08-11 09:16:20 +0000

    Added support for hash as parm on filter for more readable code + some more doc
    git-svn-id: http://svn.projects.ez.no/ezgmaplocation/trunk@22 a5a2e000-4f5a-4d95-8ad8-c7c64b196f82

--- packages/ezgmaplocation_extension/ezextension/ezgmaplocation/classes/ezgmllocationfilter.php
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/classes/ezgmllocationfilter.php
@@ -39,9 +39,12 @@ class ezgmlLocationFilter
         /*
          * Filtering nodes witin a radius of cordinates.
          * 
+         * Params can be either hash or array, in case of array this is the order:
          * param 1: latitude
          * param 2: longitude
          * param 2: distance (Optional radius length, default is 0.5)
+         * 
+         * Note: All params needs to be numeric (incl numeric string values, lik '5.5')
          * 
          * Full example for fetching nodes that are within a certain area:
          * 
@@ -51,29 +54,29 @@ class ezgmlLocationFilter
                                       'sort_by', array( 'published', false() ),
                                       'class_filter_type', 'include',
                                       'class_filter_array', array( 'user' ),
-                                      'extended_attribute_filter', hash( 'id', 'ezgmlLocationFilter', 'params', array( 59.917, 10.729 ) )
+                                      'extended_attribute_filter', hash( 'id', 'ezgmlLocationFilter', 'params', hash( 'latitude', 59.917, 'longitude', 10.729 ) )
                                       ) )}
          * 
          */
 
 
-        $latitude = isset( $params[0] ) && is_numeric( $params[0] ) ? $params[0] : 59.917;
-        $longitude = isset( $params[1] ) && is_numeric( $params[1] ) ? $params[1] : 10.729;
-        $distance = isset( $params[2] ) && is_numeric( $params[2] ) ? $params[2] : 0.5;
+        $latitude  = isset( $params['latitude'] ) && is_numeric( $params['latitude'] ) ? $params['latitude'] : ( isset( $params[0] ) && is_numeric( $params[0] ) ? $params[0] : 59.917 );
+        $longitude = isset( $params['longitude'] ) && is_numeric( $params['longitude'] ) ? $params['longitude'] : ( isset( $params[1] ) && is_numeric( $params[1] ) ? $params[1] : 10.729 );
+        $distance  = isset( $params['distance'] ) && is_numeric( $params['distance'] ) ? $params['distance'] : ( isset( $params[2] ) && is_numeric( $params[2] ) ? $params[2] : 0.5 );
 
         $minLatitude = $latitude - $distance;
         $maxLatitude = $latitude + $distance;
         $minLongitude = $longitude - $distance;
         $maxLongitude = $longitude + $distance;
 
-        $sqlJoins = "ezgmaplocation.latitude > $minLatitude AND ezgmaplocation.latitude < $maxLatitude AND
+        $sqlWhere = "ezgmaplocation.latitude > $minLatitude AND ezgmaplocation.latitude < $maxLatitude AND
                      ezgmaplocation.longitude > $minLongitude AND ezgmaplocation.longitude < $maxLongitude AND
                      ezcontentobject_attribute.id = ezgmaplocation.contentobject_attribute_id AND
                      ezcontentobject_attribute.version = ezgmaplocation.contentobject_attribute_version AND
                      ezcontentobject.id = ezcontentobject_attribute.contentobject_id AND
                      ezcontentobject.current_version = ezcontentobject_attribute.version AND";
 
-        return array('tables' => ', ezcontentobject_attribute, ezgmaplocation', 'joins' => $sqlJoins, 'columns' => '');
+        return array('tables' => ', ezcontentobject_attribute, ezgmaplocation', 'joins' => $sqlWhere, 'columns' => '');
     }
 }
 ?>
