9ceb9b177c8713762cdfca1340c2dabc2b9dedda
Łukasz Serwatka <ls@ez.no>
2009-08-12 07:48:41 +0000

    - Renamed ini file - Wrapped content in PHP comments
    git-svn-id: http://svn.projects.ez.no/ezgmaplocation/trunk@26 a5a2e000-4f5a-4d95-8ad8-c7c64b196f82

--- packages/ezgmaplocation_extension/ezextension/ezgmaplocation/settings/extendedattributefilter.ini.append.php
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/settings/extendedattributefilter.ini.append.php
@@ -1,8 +1,9 @@
-#?ini charset="utf-8"?
-
+<?php /*
 
 [ezgmlLocationFilter]
 ExtensionName=ezgmaplocation
 ClassName=ezgmlLocationFilter
 MethodName=createSqlParts
 FileName=classes/ezgmllocationfilter.php
+
+*/ ?>
