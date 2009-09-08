0514842b67fd396a5fad9bd703d9e7f3c65e5ca6
André Rømcke <ar@ez.no>
2009-09-08 13:39:25 +0000

    Added ger-DE translation contributed by xrow
    git-svn-id: http://svn.projects.ez.no/ezgmaplocation/trunk@49 a5a2e000-4f5a-4d95-8ad8-c7c64b196f82

---
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/translations/ger-DE/translation.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en">
+<context>
+    <name>extension/ezgmaplocation/datatype</name>
+    <message>
+        <location filename="ezgmaplocationtype.php" line="48"/>
+        <source>GMap Location</source>
+        <comment>Datatype name</comment>
+        <translation>GMap Ort</translation>
+    </message>
+    <message>
+        <location filename="ezgmaplocationtype.php" line="77"/>
+        <source>Missing Latitude/Longitude input.</source>
+        <translation>Längengrad/Breitengrad Eingabe fehlt.</translation>
+    </message>
+    <message>
+        <location filename="ezgmaplocationtype.php" line="84"/>
+        <source>Invalid Latitude/Longitude input.</source>
+        <translation>Längengrad/Breitengrad Eingabe ist fehlerhaft.</translation>
+    </message>
+    <message>
+        <source>Latitude</source>
+        <translation>Breitengrad</translation>
+    </message>
+    <message>
+        <source>Longitude</source>
+        <translation>Längengrad</translation>
+    </message>
+    <message>
+        <source>Update Location</source>
+        <translation>Ort aktualisieren</translation>
+    </message>
+    <message>
+        <source>Update values</source>
+        <translation>Werte aktualisieren</translation>
+    </message>
+    <message>
+        <source>Find address</source>
+        <translation>Adresse suchen</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adresse</translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation>Map</translation>
+    </message>
+</context>
+</TS>
