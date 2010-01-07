63310bb95069914a1ede5e612b02e88c4d8ea40c
Bertrand Dunogier <bd@ez.no>
2010-01-07 14:36:42 +0000

    - Fixed bug #15965: French translation for ezgmaplocation extension is missing   Translation file provided by Damien Pobel
    git-svn-id: http://svn.projects.ez.no/ezgmaplocation/stable/1.0@77 a5a2e000-4f5a-4d95-8ad8-c7c64b196f82

---
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/translations/fre-FR/translation.ts
@@ -0,0 +1,50 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezgmaplocation/datatype</name>
+    <message>
+        <source>GMap Location</source>
+        <comment>Datatype name</comment>
+        <translation>Emplacement Google Map</translation>
+    </message>
+    <message>
+        <source>Missing Latitude/Longitude input.</source>
+        <translation>Latitude/Longitude non renseignée(s)</translation>
+    </message>
+    <message>
+        <source>Invalid Latitude/Longitude input.</source>
+        <translation>Latitude/Longitude invalide(s)</translation>
+    </message>
+    <message>
+        <source>Latitude</source>
+        <translation>Latitude</translation>
+    </message>
+    <message>
+        <source>Longitude</source>
+        <translation>Longitude</translation>
+    </message>
+    <message>
+        <source>Update Location</source>
+        <translation>Mettre à jour l'emplacement</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Restaurer</translation>
+    </message>
+    <message>
+        <source>Update values</source>
+        <translation>Mettre à jour les valeurs</translation>
+    </message>
+    <message>
+        <source>Find address</source>
+        <translation>Trouver une adresse</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adresse</translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation>Carte</translation>
+    </message>
+</context>
+</TS>
