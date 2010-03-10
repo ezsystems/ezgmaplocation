6be0e44a061c75342303a23d6b07303231f76b5b
Nicolas Pastorino <nfrp@ez.no>
2010-03-10 12:07:33 +0000

    --Spanish and Catalan translations updated, credits go to Microblau, http://www.microblau.net/
    git-svn-id: http://svn.projects.ez.no/ezgmaplocation/trunk@84 a5a2e000-4f5a-4d95-8ad8-c7c64b196f82

---
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/translations/cat-ES/translation.ts
@@ -0,0 +1,54 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezgmaplocation/datatype</name>
+    <message>
+        <source>GMap Location</source>
+        <comment>Datatype name</comment>
+        <translation>Adreça GMap</translation>
+    </message>
+    <message>
+        <source>Missing Latitude/Longitude input.</source>
+        <translation>Has d&apos;introduir una longitud i una latitud.</translation>
+    </message>
+    <message>
+        <source>Invalid Latitude/Longitude input.</source>
+        <translation>Longitud/Latitud incorrecta.</translation>
+    </message>
+    <message>
+        <source>Find address</source>
+        <translation>Cerca adreça</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Restaura</translation>
+    </message>
+    <message>
+        <source>Restores location and address values to what it was on page load.</source>
+        <translation>Restaura la ubicació i l&apos;adreça.</translation>
+    </message>
+    <message>
+        <source>Latitude</source>
+        <translation>Latitud</translation>
+    </message>
+    <message>
+        <source>Longitude</source>
+        <translation>Longitud</translation>
+    </message>
+    <message>
+        <source>My current location</source>
+        <translation>La meva ubicació actual</translation>
+    </message>
+    <message>
+        <source>Gets your current position if your browser support GeoLocation and you grant this website access to it! Most accurate if you have a built in gps in your Internet device! Also note that you might still have to type in address manually!</source>
+        <translation>Troba la teva posició actual si el teu navegador suporta i té permisos per accedir a GeoLocation. Serà encara més acurat si el teu dispositiu d&apos;Internet té sistema gps integrat. Tingues en compte que es possible que tinguis que introduir l&apos;adreça manualment!</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adreça</translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation>Mapa</translation>
+    </message>
+</context>
+</TS>
---
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/translations/esl-ES/translation.ts
@@ -0,0 +1,54 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezgmaplocation/datatype</name>
+    <message>
+        <source>GMap Location</source>
+        <comment>Datatype name</comment>
+        <translation>Dirección GMap</translation>
+    </message>
+    <message>
+        <source>Missing Latitude/Longitude input.</source>
+        <translation>Falta introducir una longitud y una latitud.</translation>
+    </message>
+    <message>
+        <source>Invalid Latitude/Longitude input.</source>
+        <translation>Longitud/Latitud incorrecta.</translation>
+    </message>
+    <message>
+        <source>Find address</source>
+        <translation>Encontrar dirección</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Restaurar</translation>
+    </message>
+    <message>
+        <source>Restores location and address values to what it was on page load.</source>
+        <translation>Restaura la ubicación y la dirección.</translation>
+    </message>
+    <message>
+        <source>Latitude</source>
+        <translation>Latitud</translation>
+    </message>
+    <message>
+        <source>Longitude</source>
+        <translation>Longitud</translation>
+    </message>
+    <message>
+        <source>My current location</source>
+        <translation>Mi ubicación actual</translation>
+    </message>
+    <message>
+        <source>Gets your current position if your browser support GeoLocation and you grant this website access to it! Most accurate if you have a built in gps in your Internet device! Also note that you might still have to type in address manually!</source>
+        <translation>Encuentra tu posición actual si tu navegador soporta y tiene permisos para GeoLocation. Será más eficaz si tu dispositivo de Internet tiene gps integrado. Ten en cuenta que es posible que tengas que introducir la dirección manualmente!</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Dirección</translation>
+    </message>
+    <message>
+        <source>Map</source>
+        <translation>Mapa</translation>
+    </message>
+</context>
+</TS>
