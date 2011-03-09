673de4e971da55b232f05a5c9ccc14db287cb1df
Damien Pobel <dpobel@free.fr>
2011-03-09 16:07:16 +0800

    Localization fre-FR : added missing translations

--- packages/ezgmaplocation_extension/ezextension/ezgmaplocation/translations/fre-FR/translation.ts
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/translations/fre-FR/translation.ts
@@ -1,4 +1,6 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
 <context>
     <name>extension/ezgmaplocation/datatype</name>
     <message>
@@ -24,7 +26,7 @@
     </message>
     <message>
         <source>Update Location</source>
-        <translation>Mettre à jour l'emplacement</translation>
+        <translation type="obsolete">Mettre à jour l&apos;emplacement</translation>
     </message>
     <message>
         <source>Restore</source>
@@ -32,7 +34,7 @@
     </message>
     <message>
         <source>Update values</source>
-        <translation>Mettre à jour les valeurs</translation>
+        <translation type="obsolete">Mettre à jour les valeurs</translation>
     </message>
     <message>
         <source>Find address</source>
@@ -45,6 +47,18 @@
     <message>
         <source>Map</source>
         <translation>Carte</translation>
+    </message>
+    <message>
+        <source>Restores location and address values to what it was on page load.</source>
+        <translation>Restaurer la position et l'adresse à leur valeur au moment du chargement de la page.</translation>
+    </message>
+    <message>
+        <source>My current location</source>
+        <translation>Ma position actuelle</translation>
+    </message>
+    <message>
+        <source>Gets your current position if your browser support GeoLocation and you grant this website access to it! Most accurate if you have a built in gps in your Internet device! Also note that you might still have to type in address manually!</source>
+        <translation>Trouve votre position si votre navigateur supporte l'API de Geolocalisation and si vous autorisez à y accéder ! Cette fonctionnalité est très précise si votre appareil dispose d'une puce GPS ! Mais vous pouvez également avoir taper manuellement une adresse !</translation>
     </message>
 </context>
 </TS>
