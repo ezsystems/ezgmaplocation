f339977cf45e661b1bfdcf947192cadd9ba653e3
Patrick Allaert <pa@ez.no>
2012-05-09 16:07:17 +0200

    Changed: missing eZ copyright

--- packages/ezgmaplocation_extension/ezextension/ezgmaplocation/classes/ezgmaplocation.php
+++ packages/ezgmaplocation_extension/ezextension/ezgmaplocation/classes/ezgmaplocation.php
@@ -4,7 +4,7 @@
 //
 // SOFTWARE NAME: Blend Gmap Location Class
 // SOFTWARE RELEASE: 0.5
-// COPYRIGHT NOTICE: Copyright (C) 2006-2009 Blend Interactive
+// COPYRIGHT NOTICE: Copyright (C) 2006-2009 Blend Interactive, 1999-2012 eZ Systems AS
 // SOFTWARE LICENSE: GNU General Public License v2.0
 // NOTICE: >
 //   This program is free software; you can redistribute it and/or
